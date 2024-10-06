9d25b66ea184e9410f30b03db35df2f0247d1444
MiniMan4848 <arjigreen4@gmail.com>
2024-10-06 02:40:02 +0100

    just format nothing else

--- getDetails.js
+++ getDetails.js
@@ -3,7 +3,7 @@ import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
 puppeteer.use(StealthPlugin());
 
-const browser = await puppeteer.launch({ headless: false , slowMo: 5});
+const browser = await puppeteer.launch({ headless: false, slowMo: 10 });
 const page = await browser.newPage();
 
 export async function getDetails(url) {
@@ -12,9 +12,13 @@ export async function getDetails(url) {
   // Go to the page and wait until it's mostly idle
   await page.goto(url, { waitUntil: "networkidle2" });
 
-  const name = await getData(".align-end > div:nth-child(1) > div:nth-child(1)");
+  const name = await getData(
+    ".align-end > div:nth-child(1) > div:nth-child(1)"
+  );
   const wallet = await getData(".sm\\:text-sm");
-  const followers = await getData(".align-end > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)");
+  const followers = await getData(
+    ".align-end > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)"
+  );
   const likes = await getData(".text-red-500");
 
   await page.click(".inline-flex");
@@ -23,33 +27,45 @@ export async function getDetails(url) {
   await page.waitForSelector(".max-w-\\[400px\\]");
 
   // Fetch the initial class name of the button
-  var nextButtonClassName = await (await (await page.$("button.text-sm:nth-child(3)")).getProperty("className")).jsonValue();
+  var nextButtonClassName = await (
+    await (await page.$("button.text-sm:nth-child(3)")).getProperty("className")
+  ).jsonValue();
 
   // Loop while the button is enabled
-  if (nextButtonClassName === "text-sm text-slate-50 hover:font-bold hover:bg-transparent hover:text-slate-50") {
-    while (nextButtonClassName === "text-sm text-slate-50 hover:font-bold hover:bg-transparent hover:text-slate-50") {
-      
+  if (
+    nextButtonClassName ===
+    "text-sm text-slate-50 hover:font-bold hover:bg-transparent hover:text-slate-50"
+  ) {
+    while (
+      nextButtonClassName ===
+      "text-sm text-slate-50 hover:font-bold hover:bg-transparent hover:text-slate-50"
+    ) {
       await page.click("button.text-sm:nth-child(3)");
       // wait for page to load
       await page.waitForSelector(".max-w-\\[400px\\]");
-      var pageCount = await page.$$eval(".max-w-\\[400px\\] > a", (as) => as.length);
+      var pageCount = await page.$$eval(
+        ".max-w-\\[400px\\] > a",
+        (as) => as.length
+      );
       totalCreationCount += pageCount;
-      console.log("Page count: " + pageCount)
-      console.log("totalCreationCount: " + totalCreationCount)
-      
+      console.log("Page count: " + pageCount);
+      console.log("totalCreationCount: " + totalCreationCount);
+
       // if there are no a tags on the page, break the loop
       if (pageCount === 0) {
         break;
       }
-  }
-  
+    }
   } else {
-    var pageCount = await page.$$eval(".max-w-\\[400px\\] > a", (as) => as.length);
+    var pageCount = await page.$$eval(
+      ".max-w-\\[400px\\] > a",
+      (as) => as.length
+    );
     totalCreationCount += pageCount;
   }
 
-  console.log("totalCreationCount: " + totalCreationCount)
-  console.log("pageCount: " + pageCount)
+  console.log("totalCreationCount: " + totalCreationCount);
+  console.log("pageCount: " + pageCount);
 
   await browser.close();
 
--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import { getDetails } from "./getDetails.js";
 
 async function main() {
   console.log("starting program");
-  const test = await getDetails(
-    "https://pump.fun/profile/niggers_"
-  );
+  const test = await getDetails("https://pump.fun/profile/oduncu");
   console.log(test);
 }
 
