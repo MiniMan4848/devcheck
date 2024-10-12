47659d046057c5a70fbe09d3161516483289a939
MiniMan4848 <arjigreen4@gmail.com>
2024-10-12 21:46:25 +0100

    restarting pc

---
+++ discordbot.js
@@ -0,0 +1,83 @@
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import {
+  Client,
+  IntentsBitField,
+  ApplicationCommandOptionType,
+  REST,
+  Routes,
+} from "discord.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+puppeteer.use(StealthPlugin());
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildWebhooks,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+const commands = [
+  {
+    name: "check",
+    description: "investigate the dev of a pumpfun token",
+    options: [
+      {
+        name: "ca",
+        description: "you'll get the dev of this tokens track record",
+        type: ApplicationCommandOptionType.String,
+        required: true,
+      },
+    ],
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("registering commands");
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.APP_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+
+    console.log("commands registered");
+  } catch (error) {
+    console.error(`error: ${error}`);
+  }
+})();
+
+client.on("ready", (c) => {
+  console.log(`${c.user.tag} is online`);
+});
+
+client.on("interactionCreate", (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const CA = interaction.options.get("ca").value;
+
+  interaction.reply(`CA entered: ${CA}`);
+
+  const webScrape = async () => {
+    const browser = await puppeteer.launch({ headless: false, slowMo: 60 });
+    const page = await browser.newPage();
+
+    await page.goto(`https://pump.fun/${CA}`, { waitUntil: "networkidle2" });
+
+    await page.click(".inline-flex");
+    await page.click("#btn-accept-all");
+    await page.click(
+      "div.inline-flex > a:nth-child(2) > span:nth-child(1) > span:nth-child(2)"
+    );
+    //await browser.close();
+  };
+
+  webScrape();
+});
+
+client.login(process.env.TOKEN);
