4a8f98f3d2d403889924ea42877c4a01b130e7be
MiniMan4848 <arjigreen4@gmail.com>
2024-10-13 02:01:44 +0100

    forgot to remove unecessary files lol

--- index.js
+++
@@ -1,9 +0,0 @@
-import { getDetails } from "./getDetails.js";
-
-async function main() {
-  console.log("starting program");
-  const test = await getDetails("https://pump.fun/profile/_o_o_");
-  console.log(test);
-}
-
-main();
